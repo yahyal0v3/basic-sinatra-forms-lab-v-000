a43fcd1b119120a08d01608844280680db66b50f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-13 04:37:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @team_name = param[:name]
+    @coach = param[:coach]
+    @point_guard = param[:pg]
+    @shooting_guard = param[:sg]
+    @power_forward = param[:pf]
+    @small_forward = param[:sf]
+    @center = param[:c]
     erb :team
   end
 end
